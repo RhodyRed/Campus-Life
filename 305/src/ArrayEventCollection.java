045b38bcfced4ed04f64aa8505b64809a1e19dfe
Phil <ptan1993@gmail.com>
2015-11-15 22:37:56 -0500

    Collection

---
+++ 305/src/ArrayEventCollection.java
@@ -0,0 +1,98 @@
+import java.awt.Graphics;
+
+
+public class ArrayEventCollection implements EventCollection
+{
+	
+	private final int SIZE = 100; //Default size
+	private Event [] events;
+	private int size;
+	private int selected;
+	
+	public ArrayEventCollection()
+	{
+		events = new Event[SIZE];
+		size = 0;
+		selected = -1;
+		
+				
+	}
+	
+	/**
+	 *  changes the selected event
+	 */
+	public void changeSelection(int newSelection)
+	{
+		selected = newSelection;
+	}
+	
+	/**
+	 * adds an event
+	 */
+	public void add(Event someEvent) {
+		if(selected == events.length)
+		{
+			Event[] temp = new Event[events.length*2];
+			for(int i = 0; i<events.length; i++)
+			{
+				temp[i] = events[i];
+				events = temp;
+			}
+			events[size] = someEvent;
+			changeSelection(size++);
+		}
+		
+	}
+
+	/**
+	 * removes an event
+	 */
+
+	public void remove() 
+	{
+			for (int i = selected ; i < size-1; i++)
+				events[i] = events[i+1];
+			size--;
+			changeSelection(-1);
+		
+	}
+
+
+	/**
+	 * resets to the beginning of the collection
+	 */
+	public void reset() {
+
+		changeSelection(0);
+	}
+
+
+	public void reset(Event someEvent) {
+		
+		int i = 0;							
+		changeSelection(-1);					
+		while (selected == -1 && i < size) 
+		{
+			if (events[i] == someEvent)
+				changeSelection(i);
+			i++;
+		}
+		
+
+		
+	}
+
+
+	public boolean hasNext() {
+
+		return (size > 0) && (selected >= 0) && (selected < size);
+	}
+
+
+	public void paint(Graphics pane) {
+
+		
+	}
+	
+
+}
---
+++ 305/src/EventCollection.java
@@ -0,0 +1,27 @@
+import java.awt.Graphics;
+
+
+public interface EventCollection {
+	
+	//Adds an event
+	public void add(Event someEvent);
+	
+	
+	//Removes an event
+	public void remove();
+	
+	
+	//Resets the collection to the beginning
+	public void reset();
+	
+	//If the event is in the collection
+	//that event is the selected event
+	public void reset(Event someEvent);
+	
+	public boolean hasNext();
+	
+	public void paint(Graphics pane);
+
+	
+
+}
